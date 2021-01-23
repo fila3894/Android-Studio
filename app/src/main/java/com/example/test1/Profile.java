2b1f5b71fc0636e4ee6015ec1fa605706e659d5b
Yang Ji Hoon <70878920+fila3894@users.noreply.github.com>
2021-01-24 01:50:35 +0900

    사용자 정보 입력 화면 소스코드

--- app/src/main/java/com/example/test1/Profile.java
+++ app/src/main/java/com/example/test1/Profile.java
@@ -3,7 +3,6 @@ package com.example.test1;
 import android.location.SettingInjectorService;
 import android.os.Bundle;
 import android.widget.ImageButton;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Profile extends AppCompatActivity
