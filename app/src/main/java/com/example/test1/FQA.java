aae5bb700e1a7159d434ab8ba7f32a3f8945225c
Yang Ji Hoon <70878920+fila3894@users.noreply.github.com>
2021-01-24 01:07:59 +0900

    앱 사용 방법 설명 페이지 소스코드

--- app/src/main/java/com/example/test1/FQA.java
+++ app/src/main/java/com/example/test1/FQA.java
@@ -1,10 +1,12 @@
 package com.example.test1;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 
 public class FQA extends AppCompatActivity {
 
