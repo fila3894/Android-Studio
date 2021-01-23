e228c48371e9c15b5593ed129c7c8fa32c03856b
Yang Ji Hoon <70878920+fila3894@users.noreply.github.com>
2021-01-24 01:49:00 +0900

    메인화면 소스코드

--- app/src/main/java/com/example/test1/Main2Activity.java
+++ app/src/main/java/com/example/test1/Main2Activity.java
@@ -2,7 +2,6 @@ package com.example.test1;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
