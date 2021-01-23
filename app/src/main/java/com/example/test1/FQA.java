3df7d58c72d1cb712db154eb4597a3706bdeee9e
Yang Ji Hoon <70878920+fila3894@users.noreply.github.com>
2021-01-24 01:46:24 +0900

    앱 사용설명 방법 화면 소스코드

--- app/src/main/java/com/example/test1/FQA.java
+++ app/src/main/java/com/example/test1/FQA.java
@@ -35,6 +35,7 @@ public class FQA extends AppCompatActivity {
             }
         });
 
+        
         HowToBoot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
