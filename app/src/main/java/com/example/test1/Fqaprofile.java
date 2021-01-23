59c8468858e4f2ccecc23ddf66f4a8915cb69faf
Yang Ji Hoon <70878920+fila3894@users.noreply.github.com>
2021-01-24 01:47:21 +0900

    도움말 화면2 소스코드

--- app/src/main/java/com/example/test1/Fqaprofile.java
+++ app/src/main/java/com/example/test1/Fqaprofile.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class Fqaprofile extends AppCompatActivity {
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
